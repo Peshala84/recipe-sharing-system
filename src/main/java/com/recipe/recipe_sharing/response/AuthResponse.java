2e1e2f8bb14e873b64f282bc6480adc5a69f162a
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-05-27 22:26:21 +0530

    create new file  for response for  signup and signin part

---
+++ src/main/java/com/recipe/recipe_sharing/response/AuthResponse.java
@@ -0,0 +1,18 @@
+package com.recipe.recipe_sharing.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+
+    private String jwt;
+    private String message;
+
+}
