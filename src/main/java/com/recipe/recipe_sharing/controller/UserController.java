60560e3901a4c118db776f534c8cc56d120c3d36
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-05-27 22:36:26 +0530

    remove the delete , view and create function

--- src/main/java/com/recipe/recipe_sharing/controller/UserController.java
+++ src/main/java/com/recipe/recipe_sharing/controller/UserController.java
@@ -2,62 +2,23 @@ package com.recipe.recipe_sharing.controller;
 
 
 import com.recipe.recipe_sharing.model.User;
-import com.recipe.recipe_sharing.repository.UserRepository;
+import com.recipe.recipe_sharing.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 public class UserController {
 
     @Autowired
-     private UserRepository userRepository;
+     private UserService userService;
 
-     @PostMapping("/users")
-    public User createUser(@RequestBody User user) throws Exception {
+    @GetMapping("/api/users/profile")
+    public User findUserByJwt(@RequestHeader("Authorization") String jwt) throws Exception {
 
-         User isExist = userRepository.findByEmail(user.getEmail());
-         if( isExist != null ){
-               throw new Exception("User is Exist with this email: " + user.getEmail());
-         }
-    User savedUser = userRepository.save(user);
-          return  savedUser;
+        User user= userService.findUserByJwt(jwt);
+
+        return user;
+
     }
-
-    @DeleteMapping("/users/{userId}")
-    public String deleteUser(@PathVariable Long userId) throws Exception {
-
-         userRepository.deleteById(userId);
-        return "User Deleted Successfully" ;
-    }
-
-    @GetMapping("/users")
-    public List<User> getAllUsers() throws Exception {
-
-        List<User> users = userRepository.findAll();
-        return users ;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    public User  findByEmail(String email) throws Exception {
-//
-//    User user = userRepository.findByEmail(email);
-//     if(user == null ){
-//         throw new Exception("User Not Found with email: " + email);
-//     }
-//     return user;
-//     }
 }
