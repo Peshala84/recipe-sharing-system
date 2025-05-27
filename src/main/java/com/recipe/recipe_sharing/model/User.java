8de7a41512263e5166237de5d6d90e7f6bf99fed
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-05-27 22:35:02 +0530

    update this file don't see password in user profile

--- src/main/java/com/recipe/recipe_sharing/model/User.java
+++ src/main/java/com/recipe/recipe_sharing/model/User.java
@@ -1,6 +1,7 @@
 package com.recipe.recipe_sharing.model;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,6 +18,7 @@ public class User{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private String email;
     private String fullName;
