766f3c2dadb0ed1ec761463da9c6c658ec12ad9d
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-05-30 21:25:59 +0530

    create foe this file display the recipe card

--- recipe-sharing/src/app/pages/recipe-card/recipe-card.component.ts
+++ recipe-sharing/src/app/pages/recipe-card/recipe-card.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatDialog } from '@angular/material/dialog';
+import { UpdateRecipeFormComponent } from '../update-recipe-form/update-recipe-form.component';
+import { RecipeServiceService } from '../../services/Recipe/recipe-service.service';
 
 @Component({
   selector: 'app-recipe-card',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './recipe-card.component.html',
   styleUrl: './recipe-card.component.scss'
 })
 export class RecipeCardComponent {
 
+  @Input() recipe: any
+  @Input() toggle: any
+
+  constructor(public dialog: MatDialog, private recipeService: RecipeServiceService) {
+
+  }
+
+  handleopenEditRecipeForm() {
+    this.dialog.open(UpdateRecipeFormComponent, {
+      data: this.recipe
+    })
+  }
+  ngOnInit() {
+    console.log("toggle", this.toggle)
+  }
+  handleDeleteRecipe() {
+    this.recipeService.deleteRecipes(this.recipe.id).subscribe()
+  }
 }
