fe1e5da2d8b1feb50f097a1826ccbf61c9d4dd3e
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-05-30 21:24:54 +0530

    updated navbar compnent

--- recipe-sharing/src/app/pages/navbar/navbar.component.ts
+++ recipe-sharing/src/app/pages/navbar/navbar.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { AuthServiceService } from '../../services/Auth/auth-service.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -11,5 +13,21 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  user: any = null;
 
+  constructor(public authService: AuthServiceService,private router:Router) { }
+
+  ngOnInit() {
+    this.authService.authSubject.subscribe(
+      (auth) => {
+        console.log("auth object valur", auth)
+        this.user = auth.user
+      }
+    )
+
+  }
+   handleLogOut(){
+    this.authService.logout
+    // this.router.navigate("")
+  }
 }
