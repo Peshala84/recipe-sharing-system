235d2e4d58257026d99bcf13a5a4e98abb618b9a
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-05-30 21:21:44 +0530

    update app component file also connect backend

--- recipe-sharing/src/app/app.component.ts
+++ recipe-sharing/src/app/app.component.ts
@@ -6,12 +6,37 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { NavbarComponent } from './pages/navbar/navbar.component';
 import { FooterComponent } from './pages/footer/footer.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { AuthServiceService } from './services/Auth/auth-service.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent,HomePageComponent,AuthComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'recipe-sharing';
+
+  user:any=null;
+  
+  constructor(public authService:AuthServiceService){}
+
+   ngOnInit() {
+    console.log("ngOnInit")
+    this.authService.getUserProfile().subscribe({
+      next:data=>console.log("ureq user",data),
+      error:error=>console.log("error",error)
+      
+
+    });
+    this.authService.authSubject.subscribe(
+      (auth)=>{
+        console.log("auth object valur", auth)
+        this.user=auth.user
+      }
+    )
+  }
+}
